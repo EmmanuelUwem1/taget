5fcd76c24e31e591835f7ead875c6d05e7f0d392
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-05-31 17:49:14 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import logo from "/logo.png"
 
 function Header() {
   return (
-    <header className="px-4 sm:px-8 md:px-16 py-4 sm:py-8 md:py-10 flex justify-center items-center w-full">
+    <header className="px-4 sm:px-8 md:px-16 lg:px-20 xl:px-24 py-4 sm:py-8 md:py-10 flex justify-center items-center w-full">
       {/* the heade with border */}
       <div className=" py-4 sm:px-8 md:px-16 flex justify-between items-center rounded-full w-full border border-[#000000]">
         <span className="relative flex items-center justify-center h-6 w-20">
@@ -13,13 +13,13 @@ function Header() {
           />
         </span>
         {/* Navigation links can be added here */}n
-        <nav className="gap-20 flex justify-center items-center text-[#0D0D0D]">
+        <nav className="hidden lg:flex gap-20 justify-center items-center text-[#0D0D0D]">
           <a href="/">Home</a>
           <a href="/">About Us</a>
           <a href="/">Events</a>
           <a href="/">Contact</a>
         </nav>
-        <button className="bg-[#0D0D0D] rounded-[6px] py-3.5 px-6 transition-class hover:opacity-80">Join Us</button>
+        <button className="bg-[#0D0D0D] rounded-[6px] py-3.5 px-6 transition-class hover:opacity-80 cursor-pointer">Join Us</button>
       </div>
     </header>
   );
--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,8 +1,38 @@
-    
+import heroImage from "/hero-image.png"
 function Hero() {
   return (
-    <section className='flex justify-between items-center gap-8'></section>
-  )
+    <section className="flex justify-between items-center gap-8 h-screen lg:flex-nowrap flex-wrap py-8 text-black px-4 sm:px-8 md:px-16 lg:px-20 xl:px-24">
+      <div className="flex flex-col justify-start items-center w-full lg:w-1/2">
+        <div>
+          <span></span>
+          <span></span>
+          <span></span> 200+
+        </div>
+        <div>
+          <h1>Set Your Targets with Taget</h1>
+        </div>
+
+        <div>
+          <p>
+            Empowering communities, igniting innovation, and building a
+            decentralized future—one milestone at a time.
+          </p>
+        </div>
+        <div>
+          <button className="bg-[#0D0D0D] text-white rounded-[6px] py-3.5 px-6 transition-class hover:opacity-80 cursor-pointer">
+            Get Involved Now
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center items-center h-fit w-[80%] lg:w-[50%]">
+        <img
+          src={heroImage}
+          alt="hero image"
+          className="object-contain object-center"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Hero
